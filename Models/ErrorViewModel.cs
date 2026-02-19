be5d5018b48e03be994f5ce2a47671e789402d5d
rahul05-code <kanzariyarahul31@gmail.com>
2026-02-19 07:47:23 -0500

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace HandMotionPlay_.net.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
