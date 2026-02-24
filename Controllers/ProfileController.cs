8f63ddd408984e7a17780300df1437c103e33649
rahul05-code <kanzariyarahul31@gmail.com>
2026-02-24 03:10:19 -0500

    game , profile , progress complete

---
+++ Controllers/ProfileController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace HandMotionPlay_.net.Controllers
+{
+    public class ProfileController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Profile()
+        {
+            return View();
+        }
+    }
+}
